dcdef83017b7ad4aa2fef87926ce33c6162b623a
Chris Hartjes <chartjes@littlehart.net>
2018-04-30 09:28:41 -0400

    Removed code no longer being used

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,9 @@
 import os
 import pytest
-import ssl
 
 from dotenv import load_dotenv, find_dotenv
 from pytest_bugzilla_notifier.bugzilla_rest_client import BugzillaRESTClient
 
-
-# Hack because of how SSL certificates are verified by default in Python
-'''
-if hasattr(ssl, '_create_unverified_context'):
-    ssl._create_default_https_context = ssl._create_unverified_context
-'''
 
 # Check to see if we have a dotenv file and use it
 if find_dotenv():
@@ -25,4 +18,3 @@ def bugzilla_secure_user_one():
     }
     client = BugzillaRESTClient(api_details)
     return client
-
